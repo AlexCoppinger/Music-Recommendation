f4caf384cbed6bf246d7cc991089013f7bc1f37f
AlexCoppinger <afcoppinger@ucdavis.edu>
2025-04-13 14:50:40 -0700

    yipee

---
+++ musicrec/vibelink/spotify.py
@@ -0,0 +1,12 @@
+import spotipy
+from spotipy.oauth2 import SpotifyOAtuh, SpotifyClientCredentials
+
+#We want our Django app to:
+# Authenticate itself with Spotify using the Client Credentials Flow
+# Use the Spotify API to request public data
+# Return that data via Django views or use it inside your app
+
+# We use this spotipy to wrap the Spotify API in Python functions
+# Use this module to organize the API logic
+# Django views to interact with users and return data
+
