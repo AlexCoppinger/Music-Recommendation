e429a9849881a06e3a652833180523b935ddbd50
AlexCoppinger <afcoppinger@ucdavis.edu>
2025-06-12 15:46:36 -0700

    added favicon

--- musicrec/vibelink/views.py
+++ musicrec/vibelink/views.py
@@ -16,30 +16,6 @@ import spotipy
 import uuid
 import json
 
-# The following function are irrelevant to our app now
-# They're just here for reference:
-
-# def playlist_search_view(request):
-#     print("search_spotify_playlists called")
-#     query = request.GET.get('query', '')
-#     playlists = []
-#     if query:
-#         try:
-#             playlists = search_playlists(query)
-#         except Exception as e:
-#             return render(request, 'vibelink/search.html', {'error': str(e)})
-#     return render(request, 'vibelink/search.html', {'playlists': playlists})
-
-# def track_search_view(request):
-#     query = request.GET.get('query', '')
-#     tracks = []
-#     if query:
-#         try:
-#             tracks = search_tracks(query)
-#         except Exception as e:
-#             return render(request, 'vibelink/search.html', {'error': str(e)})
-#     return render(request, 'vibelink/search.html', {'tracks': tracks})
-
 def home(request):
     return render(request, 'vibelink/home.html')
 
